2fac0a3c586bf12af84ebb422561c1d7e72dad97
masanag <masaki.nagaishi@gmail.com>
2016-04-23 18:53:42 +0900

    done 9.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 #
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
   end
@@ -39,5 +41,17 @@ class UsersController < ApplicationController
     params
       .require(:user)
       .permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: 'Please sign in.' unless signed_in?
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_path unless current_user?(@user)
   end
 end
