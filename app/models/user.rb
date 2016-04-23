cd7efd07b0af37e8ca4299576a2cafe1ea3b1427
masanag <masaki.nagaishi@gmail.com>
2016-04-24 01:42:51 +0900

    done 10.1.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,25 @@
+#
 class User < ActiveRecord::Base
+  has_many :microposts
   before_save { self.email = email.downcase }
   before_create :create_remember_token
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :email,
+            presence: true,
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
 
-  def User.new_remember_token
-    SecureRandom.urlsafe_base64
-  end
+  class << self
+    def new_remember_token
+      SecureRandom.urlsafe_base64
+    end
 
-  def User.encrypt(token)
-    Digest::SHA1.hexdigest(token.to_s)
+    def encrypt(token)
+      Digest::SHA1.hexdigest(token.to_s)
+    end
   end
 
   private
