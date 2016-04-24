a1c6da26d19f2d4db224a2bd8315d8d289fa6b2b
masanag <masaki.nagaishi@gmail.com>
2016-04-24 18:02:30 +0900

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 RailstutorialSampleAppRails2nd03::Application.routes.draw do
-  get "microposts/create"
-  get "microposts/destroy"
+  get 'microposts/create'
+  get 'microposts/destroy'
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
