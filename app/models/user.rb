7da4993f8d511924d0bc3831239a8593e5f2e0b8
masanag <masaki.nagaishi@gmail.com>
2016-04-24 17:49:14 +0900

    done 10.3.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,10 @@ class User < ActiveRecord::Base
     end
   end
 
+  def feed
+    Micropost.where('user_id = ?', id)
+  end
+
   private
 
   def create_remember_token
--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Micropost do
   let(:user) { create :user }
   before do
-    @micropost = Micropost.new(content: 'Lorem ipsum', user_id: user.id)
+    @micropost = user.microposts.new(content: 'Lorem ipsum', user_id: user.id)
   end
 
   subject { @micropost }
@@ -17,6 +17,16 @@ describe Micropost do
 
   describe 'when user_id is not present' do
     before { @micropost.user_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe 'when blank content' do
+    before { @micropost.content = '' }
+    it { should_not be_valid }
+  end
+
+  describe 'with content that is too long' do
+    before { @micropost.content = 'a' * 141 }
     it { should_not be_valid }
   end
 end
